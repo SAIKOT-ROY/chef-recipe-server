ee119f8917c958012274deef04d3d2069c658a14
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2023-05-02 19:21:16 +0600

    chef json data addded and created a json format

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json')
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Valo asi valo takho')
+});
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+
+app.listen(port, () => {
+    console.log(`Assignment pera dei ${port}`);
+})
